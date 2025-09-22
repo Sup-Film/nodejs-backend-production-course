23c487ec012d9ae77284a46c692032d2f5b1d837
Sup-Film <sup.dongkew@gmail.com>
2025-09-22 13:04:52 +0700

    feat: initialize project with basic structure and routes

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello, World!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
